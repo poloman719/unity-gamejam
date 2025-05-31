02d6a3b19c2afebeb3fb90eb13056cca68f0df59
Jeremiah de Guzman <jeremiahdeguzman@Jeremiahs-MacBook-Air.local>
2025-05-31 00:57:46 -0700

    red wizard fan projectiles

---
+++ gamejam-bullethell/Assets/ProjectileScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb;
+    public GameObject wizardObject;
+
+    void Start()
+    {
+        wizardObject = GameObject.FindWithTag("Wizard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.position.y < -7) Destroy(gameObject);
+    }
+}
---
+++ gamejam-bullethell/Assets/WizardScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System;
+
+public class WizardScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb;
+    public GameObject projectileObject;
+    double timer = 0;
+    // public Vector2 projectileVelocity = Vector2.zero;
+
+    void Start()
+    {
+        projectileObject = GameObject.FindWithTag("Projectile");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            timer = 0;
+            double angleDelta = 20;
+            shootProjectile(270);
+            shootProjectile(270 + angleDelta);
+            shootProjectile(270 + -angleDelta);
+            shootProjectile(270 + 2 * angleDelta);
+            shootProjectile(270 + -2 * angleDelta);
+            shootProjectile(270 + 3 * angleDelta);
+            shootProjectile(270 + -3 * angleDelta);
+        }
+    }
+
+    void shootProjectile(double angle)
+    {
+        // projectileVelocity = new Vector2((float)Math.Cos(angle * Math.PI / 180), (float)Math.Sin(angle * Math.PI / 180)) * 15;
+        GameObject newObject = GameObject.Instantiate(projectileObject, rb.position, Quaternion.identity);
+        newObject.GetComponent<Rigidbody2D>().linearVelocity = new Vector2((float)Math.Cos(angle * Math.PI / 180), (float)Math.Sin(angle * Math.PI / 180)) * 15;
+    }
+}
