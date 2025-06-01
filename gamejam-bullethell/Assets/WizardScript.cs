fa0e8ed8f52cde05c2ebc8ebab8f9fdd835542ae
NotK <lkyle8228@gmail.com>
2025-06-01 16:04:04 -0700

    wizard sine wave movement half done

--- gamejam-bullethell/Assets/WizardScript.cs
+++ gamejam-bullethell/Assets/WizardScript.cs
@@ -9,7 +9,10 @@ public class WizardScript : MonoBehaviour
     double timer = 0;
 
     public float bulletSpeed;
-    
+
+    public int moveDirection = 0; // 0 -> not moving, -1 -> left, 1 -> right
+    double moveTimer = 0;
+
     // public Vector2 projectileVelocity = Vector2.zero;
 
     void Start()
@@ -20,26 +23,50 @@ public class WizardScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         timer += Time.deltaTime;
+        moveTimer += Time.deltaTime;
+        moveWave();
         if (timer > 1)
         {
             timer = 0;
             double angleDelta = 20;
             shootProjectile(270);
             shootProjectile(270 + angleDelta);
-            shootProjectile(270 + -angleDelta); 
+            shootProjectile(270 + -angleDelta);
             shootProjectile(270 + 2 * angleDelta);
             shootProjectile(270 + -2 * angleDelta);
             shootProjectile(270 + 3 * angleDelta);
             shootProjectile(270 + -3 * angleDelta);
         }
+        
     }
 
     void shootProjectile(double angle)
     {
-        // projectileVelocity = new Vector2((float)Math.Cos(angle * Math.PI / 180), (float)Math.Sin(angle * Math.PI / 180)) * 15;
+        int prevMove = moveDirection;
+        moveDirection = 0;
         GameObject newObject = GameObject.Instantiate(projectileObject, rb.position, Quaternion.identity);
         newObject.GetComponent<Rigidbody2D>().linearVelocity = new Vector2((float)Math.Cos(angle * Math.PI / 180), (float)Math.Sin(angle * Math.PI / 180)) * bulletSpeed;
-        newObject.transform.Rotate(new Vector3(0,0, (float) angle + 90));
+        newObject.transform.Rotate(new Vector3(0, 0, (float)angle + 90));
+        moveDirection = prevMove;
+    }
+
+    [ContextMenu("Attack 1 - Spray")]
+    public void sprayAttack()
+    {
+        double angleDelta = 20;
+        shootProjectile(270);
+        shootProjectile(270 + angleDelta);
+        shootProjectile(270 + -angleDelta);
+        shootProjectile(270 + 2 * angleDelta);
+        shootProjectile(270 + -2 * angleDelta);
+        shootProjectile(270 + 3 * angleDelta);
+        shootProjectile(270 + -3 * angleDelta);
+    }
+
+    void moveWave()
+    {
+        rb.linearVelocity = new Vector2(moveDirection, (float) Math.Sin(moveTimer));
     }
 }
