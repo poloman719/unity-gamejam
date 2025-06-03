0df90c032c33d3bf2b95e5d4d6bde665763aaae9
NotK <lkyle8228@gmail.com>
2025-06-03 11:18:25 -0700

    finished beam animation implementation

--- gamejam-bullethell/Assets/Scripts/YellowBeamScript.cs
+++ gamejam-bullethell/Assets/Scripts/YellowBeamScript.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class YellowBeamScript : MonoBehaviour
 {
+    public double fireTime;
+    double timer;
+    public Animator anim;
+    public GameObject itself;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,14 @@ public class YellowBeamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > fireTime)
+        {
+            anim.SetTrigger("Ending");
+        }
+        if (timer > (fireTime + 1.542))
+        {
+            Destroy(itself);
+        }
     }
 }
