29e0ab2347ea5a0618fd4551fc31d8623ecf3875
l6kshy6 <l6kshy6@gmail.com>
2025-06-03 20:34:30 -0700

    added music, music & SFX sliders

--- gamejam-bullethell/Assets/Scripts/AdjustVol.cs
+++ gamejam-bullethell/Assets/Scripts/AdjustVol.cs
@@ -8,4 +8,18 @@ public class AdjustVol : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
+
+    private void Start(){
+        SetMusicVol();
+        SetSFXVol();
+    }
+
+    public void SetSFXVol(){
+        float vol = sfxSlider.value;
+        myMixer.SetFloat("SFXVol", Mathf.Log10(vol)*20);
+    }
+    public void SetMusicVol(){
+        float vol = musicSlider.value;
+        myMixer.SetFloat("MusicVol", Mathf.Log10(vol)*20);
+    }
 }
