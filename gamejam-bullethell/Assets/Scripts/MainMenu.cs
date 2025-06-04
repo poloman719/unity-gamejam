75555c5ed50d954a00f7ab5304ac2e0c5d87bd99
poloman719 <92735647+poloman719@users.noreply.github.com>
2025-06-04 04:44:20 -0700

    fixed the title screen and setup gameplay scene

--- gamejam-bullethell/Assets/Scripts/MainMenu.cs
+++ gamejam-bullethell/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public double pressedTime = 0.5;
+    public double transitionTime = 10;
+    public Animator animator;
+    public Sprite pressedSprite;
+    double timer;
+    int state = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (state == 1 && timer > pressedTime)
+        {
+            state = 2;
+            timer = 0;
+            animator.SetTrigger("Start");
+            Debug.Log("moving to state 2");
+        }
+        if (state == 2 && timer > transitionTime)
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(1);
-        SceneManager.LoadSceneAsync("Intro Scene");
+        GetComponent<Image>().sprite = pressedSprite;
+        state = 1;
+        timer = 0;
+        Debug.Log("moving to state 1");
+        animator.SetBool("Press", true);
     }
 
     public void QuitGame()
